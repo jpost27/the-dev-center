1a1b3879e9a7aa0b8c33fba00b5fc09721115ec2
Josh Post <joshpost94@gmail.com>
2022-12-19 14:41:57 -0500

    initial project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'angular-quickstart';
+  title = 'dev-center';
 }
